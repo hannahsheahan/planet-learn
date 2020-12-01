0c2ef15d30ca87699db4ae5bffd6f5b773f40452
Fabian Otto <fabianotto267@gmail.com>
2020-12-01 14:54:21 +0100

    added first version of single hidden-layer linear network to solve the task

--- starter_code/network.py
+++ starter_code/network.py
@@ -1,4 +1,3 @@
-
 """
 Author: Hannah Sheahan, sheahan.hannah@gmail.com
 Date: 19/11/2020
@@ -19,7 +18,6 @@ from torch.utils.tensorboard import SummaryWriter
 import json
 import random
 from datetime import datetime
-from IPython import embed
 
 
 class CreateDataset(Dataset):
@@ -33,8 +31,11 @@ class CreateDataset(Dataset):
         """
         self.index = dataset['index']
         self.label = dataset['label']
-        self.input_features = dataset['input_features']
-        self.data = {'index':self.index, 'label':self.label,  'input_features':self.input_features}
+        self.input_item = dataset['input_item']
+        self.input_context = dataset['input_context']
+        self.words = dataset['words']
+        self.domains = dataset['domains']
+        self.data = {'index':self.index, 'label':self.label,  'input_item':self.input_item, 'input_item':self.input_context}
 
     def __len__(self):
         return len(self.index)
@@ -43,7 +44,7 @@ class CreateDataset(Dataset):
         # for retrieving either a single sample of data, or a subset of data
         if torch.is_tensor(idx):
             idx = idx.tolist()
-        sample = {'index':self.index[idx], 'label':self.label[idx].flatten(), 'input_features':self.input_features[idx]}
+        sample = {'index':self.index[idx], 'label':self.label[idx].flatten(), 'input_item':self.input_item[idx], 'input_context':self.input_context[idx]}
         return sample
 
 
@@ -62,9 +63,8 @@ def train(args, model, device, train_loader, optimizer, criterion, epoch, printO
     correct = 0
     for batch_idx, data in enumerate(train_loader):
         optimizer.zero_grad()   # zero the parameter gradients
-        input_features, labels = batch_to_torch(data['input_features']), data['label'].type(torch.FloatTensor)
-        # embed()
-        output = model(input_features)
+        input_item, input_context, labels = batch_to_torch(data['input_item']), batch_to_torch(data['input_context']), data['label'].type(torch.FloatTensor)
+        output = model(input_item, input_context)
         output = np.squeeze(output, axis=1)
 
         loss = criterion(output, labels)
@@ -91,8 +91,8 @@ def test(args, model, device, test_loader, criterion, printOutput=True):
 
     with torch.no_grad():  # dont track the gradients
         for batch_idx, data in enumerate(test_loader):
-            input_features, labels = batch_to_torch(data['input_features']), data['label'].type(torch.FloatTensor)
-            output = model(input_features)
+            input_item, input_context, labels = batch_to_torch(data['input_item']), batch_to_torch(data['input_context']), data['label'].type(torch.FloatTensor)
+            output = model(input_item, input_context)
             output = np.squeeze(output, axis=1)
             test_loss += criterion(output, labels).item()
 
@@ -106,28 +106,47 @@ def test(args, model, device, test_loader, criterion, printOutput=True):
     return test_loss, accuracy
 
 
-class Net(nn.Module):
+class RM_Net(nn.Module):
     """
-    This is a network with the 'inverse' architecture to the one in Saxe et al. (2019) PNAS.
+    This is a network with the architecture shown in Rogers/McClelland 2008.
     """
-    def __init__(self, D_features_in, D_out, D_h_features):
-        super(Net, self).__init__()
-        self.h_features_size = D_h_features
+    def __init__(self, D_item_in, D_context_in, D_out, D_h_item, D_h_context, D_h_combined):
+        super(RM_Net, self).__init__()
+        self.h_item_size = D_h_item
+        self.h_context_size = D_h_context
+        self.h_combined_size = D_h_combined
 
-        self.fc_feature_to_hfeatures = nn.Linear(D_features_in, self.h_features_size)  # size input, size output
-        self.fc_hfeature_to_out  = nn.Linear(self.h_features_size, D_out)
+        self.fc_item_to_hitem = nn.Linear(D_item_in, self.h_item_size)  # size input, size output
+        self.fc_context_to_hcontext = nn.Linear(D_context_in, self.h_context_size)
+        self.fc_hitem_to_combined = nn.Linear(self.h_item_size, self.h_combined_size)
+        self.fc_hcontext_to_combined = nn.Linear(self.h_context_size, self.h_combined_size)
+        self.fc_combined_to_out = nn.Linear(self.h_combined_size, D_out)
 
-    def forward(self, x_feature):
+    def forward(self, x_item, x_context):
 
-        # just linear activations with MSE loss:
-        self.hfeature_activations = self.fc_feature_to_hfeatures(x_feature)
-        self.output = self.fc_hfeature_to_out(self.hfeature_activations)
+        # standard relu throughout case (use with MSE loss)
+        #self.hitem_activations = F.relu(self.fc_item_to_hitem(x_item))
+        #self.hcontext_activations = F.relu(self.fc_context_to_hcontext(x_context))
+        #self.combined_activations = F.relu(self.fc_hitem_to_combined(self.hitem_activations) + self.fc_hcontext_to_combined(self.hcontext_activations))
+        #self.output = F.relu(self.fc_combined_to_out(self.combined_activations))
+
+        # alternative linear + sigmoid at end case (use with BCE loss)
+        self.hitem_activations = self.fc_item_to_hitem(x_item)
+        self.hcontext_activations = self.fc_context_to_hcontext(x_context)
+        self.combined_activations = self.fc_hitem_to_combined(self.hitem_activations) + self.fc_hcontext_to_combined(self.hcontext_activations)
+        self.output = torch.sigmoid(self.fc_combined_to_out(self.combined_activations))
+
+        # alternative relu + sigmoid on final layer (use with BCE loss)
+        #self.hitem_activations = F.relu(self.fc_item_to_hitem(x_item))
+        #self.hcontext_activations = F.relu(self.fc_context_to_hcontext(x_context))
+        #self.combined_activations = F.relu(self.fc_hitem_to_combined(self.hitem_activations) + self.fc_hcontext_to_combined(self.hcontext_activations))
+        #self.output = torch.sigmoid(self.fc_combined_to_out(self.combined_activations))
 
         return self.output
 
-    def get_activations(self, x_feature):
-        self.forward(x_feature)  # update the activations with the particular input
-        return self.hfeature_activations
+    def get_activations(self, x_item, x_context):
+        self.forward(x_item, x_context)  # update the activations with the particular input
+        return self.combined_activations, self.hcontext_activations, self.hitem_activations
 
 
 def init_weights(m):
@@ -135,7 +154,7 @@ def init_weights(m):
       - Keep gain small to ensure small and low variance weights.
       """
     if type(m) == nn.Linear:
-        gain = 0.00001
+        gain = 1.0
         torch.nn.init.xavier_uniform(m.weight, gain)
 
 
@@ -161,21 +180,25 @@ def get_activations(args, trained_model, test_loader):
     """ This will determine the hidden unit activations for each input pair in the train/test set."""
 
     #  pass each input through the network and see what happens to the hidden layer activations
-    item_activations = np.zeros((args.n_unique, args.D_h_features)) # should strictly be called sth like feature_activations
+    item_activations = np.zeros((args.n_unique, args.D_h_item))
+    context_activations = np.zeros((args.n_unique, args.D_h_context))
+    combined_activations = np.zeros((args.n_unique, args.D_h_combined))
     trained_model.eval()
     with torch.no_grad():
         for sample_idx, data in enumerate(test_loader):
-            input_features, labels = batch_to_torch(data['input_features']), data['label'].type(torch.FloatTensor)
-            item_act = trained_model.get_activations(input_features)
+            input_item, input_context, labels = batch_to_torch(data['input_item']), batch_to_torch(data['input_context']), data['label'].type(torch.FloatTensor)
+            combined_act, context_act, item_act = trained_model.get_activations(input_item, input_context)
             item_activations[sample_idx] = item_act
+            context_activations[sample_idx] = context_act
+            combined_activations[sample_idx] = combined_act
 
-    activations = [item_activations]
+    activations = [item_activations, context_activations, combined_activations]
     return activations
 
 
 def get_model_name(args):
     """Determine the correct name for the model and analysis files."""
-    hiddensizes = '_' + str(args.D_h_features)
+    hiddensizes = '_' + str(args.D_h_item) + '_' + str(args.D_h_context) + '_' + str(args.D_h_combined)
     model_name = const.MODEL_DIRECTORY + str(args.id) + '_model' + hiddensizes + '.pth'
     analysis_name = const.ANALYSIS_DIRECTORY + str(args.id) + '_model_analysis' + hiddensizes + '.npy'
     return model_name, analysis_name
@@ -191,11 +214,11 @@ def train_network(args, device, trainset, testset):
     print("\n")
 
     # Define a model for training
-    model = Net(args.D_features_in, args.D_out, args.D_h_features).to(device)
+    model = RM_Net(args.D_item_in, args.D_context_in, args.D_out, args.D_h_item, args.D_h_context, args.D_h_combined).to(device)
     model.apply(init_weights)
 
-    criterion = nn.MSELoss()   # mean squared error loss
-    #criterion = nn.BCELoss() # binary cross entropy loss
+    #criterion = nn.MSELoss()   # mean squared error loss
+    criterion = nn.BCELoss() # binary cross entropy loss
     optimizer = optim.SGD(model.parameters(), lr=args.lr, momentum=args.momentum, weight_decay=args.weight_decay)
 
     # Define our dataloaders
@@ -209,7 +232,7 @@ def train_network(args, device, trainset, testset):
     # Log the model on TensorBoard and label it with the date/time and some other naming string
     now = datetime.now()
     date = now.strftime("_%d-%m-%y_%H-%M-%S")
-    comment = "_lr-{}_epoch-{}_hfeatures{}".format(args.lr, args.epochs, args.D_h_features)
+    comment = "_lr-{}_epoch-{}_hitem{}_hcxt{}_hcomb{}".format(args.lr, args.epochs, args.D_h_item, args.D_h_context, args.D_h_combined)
     writer = SummaryWriter(log_dir=const.TB_LOG_DIRECTORY + 'record_' + date + comment)
     print("Open tensorboard in another shell to monitor network training (hannahsheahan$  tensorboard --logdir=training_records/tensorboard)")
 
@@ -240,8 +263,7 @@ def train_network(args, device, trainset, testset):
     args.id = randnum
     model_name, _ = get_model_name(args)
     dat = json.dumps(record)
-    path = const.TRAININGRECORDS_DIRECTORY + randnum + date + comment + ".json"
-    f = open(path,"w")
+    f = open(const.TRAININGRECORDS_DIRECTORY + randnum + date + comment + ".json","w")
     f.write(dat)
     f.close()
 
@@ -252,4 +274,4 @@ def train_network(args, device, trainset, testset):
     print(model_name)
     torch.save(model, model_name)
 
-    return model, randnum, path
+    return model, randnum
